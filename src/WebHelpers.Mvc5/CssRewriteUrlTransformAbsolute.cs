2fbdf8fd6a0203d5f69896ff2836f1e982be3eff
Connor O'Shea <connor@connoroshea.com>
2017-07-23 17:40:54 -0400

    Rename css rewrite and add nuspec

---
+++ src/WebHelpers.Mvc5/CssRewriteUrlTransformAbsolute.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebHelpers.Mvc5
+{
+    /// <summary>
+    /// Rewrites URLs to be absolute so assets will still be found after bundling.
+    /// </summary>
+    public class CssRewriteUrlTransformAbsolute : IItemTransform
+    {
+        /// <summary>
+        /// Converts any URLs in the input to absolute using the application's base directory.
+        /// </summary>
+        /// <param name="includedVirtualPath">The virtual path that was included in the bundle for this item that is being transformed.</param>
+        /// <param name="input">
+        /// The input to be rewritten. For example, url(../fonts/glyphicons.woff) is rewritten as
+        /// url(Contoso/Content/fonts/glyphicons.woff) for an application whose base directory is Contoso.
+        /// </param>
+        public string Process(string includedVirtualPath, string input)
+        {
+            return new CssRewriteUrlTransform().Process("~" + VirtualPathUtility.ToAbsolute(includedVirtualPath), input);
+        }
+    }
+}
