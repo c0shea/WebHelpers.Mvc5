0678c2c4fd9a6894e3149b8e8b697a890b45e463
Connor O'Shea <connor@connoroshea.com>
2018-05-03 22:29:39 -0400

    Add ReSharper annotations so Url.IsLinkActive has IntelliSense with action and controller parameters

--- src/WebHelpers.Mvc5/UrlHelperExtensions.cs
+++ src/WebHelpers.Mvc5/UrlHelperExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using JetBrains.Annotations;
 
 namespace WebHelpers.Mvc5
 {
@@ -8,17 +9,17 @@ namespace WebHelpers.Mvc5
     public static class UrlHelperExtensions
     {
         /// <summary>
-        /// Gets the CSS class to use for the link state of the specified <paramref name="action"/>.
-        /// If the current request route matches the <paramref name="action"/> and <paramref name="controller"/>,
+        /// Gets the CSS class to use for the link state of the specified <paramref name="actionName"/>.
+        /// If the current request route matches the <paramref name="actionName"/> and <paramref name="controllerName"/>,
         /// the "active" class is returned.
         /// </summary>
         /// <param name="url">The <see cref="UrlHelper"/>.</param>
-        /// <param name="action">The action of the link to compare to the current request.</param>
-        /// <param name="controller">The controller of the link to compare to the current request.</param>
-        public static string IsLinkActive(this UrlHelper url, string action, string controller)
+        /// <param name="actionName">The action of the link to compare to the current request.</param>
+        /// <param name="controllerName">The controller of the link to compare to the current request.</param>
+        public static string IsLinkActive(this UrlHelper url, [AspMvcAction] string actionName, [AspMvcController] string controllerName)
         {
-            if (url.RequestContext.RouteData.Values["controller"].ToString() == controller &&
-                url.RequestContext.RouteData.Values["action"].ToString() == action)
+            if (url.RequestContext.RouteData.Values["controller"].ToString() == controllerName &&
+                url.RequestContext.RouteData.Values["action"].ToString() == actionName)
             {
                 return "active";
             }
