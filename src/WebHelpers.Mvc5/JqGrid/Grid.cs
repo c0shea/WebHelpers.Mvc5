aa80fd6e02a20b4ba6b5396244a6ad71432a3a70
Connor O'Shea <connor@connoroshea.com>
2018-05-13 22:35:08 -0400

    Add more grid options

--- src/WebHelpers.Mvc5/JqGrid/Grid.cs
+++ src/WebHelpers.Mvc5/JqGrid/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,82 @@ using Newtonsoft.Json;
 
 namespace WebHelpers.Mvc5.JqGrid
 {
+    /// <remarks>
+    /// http://www.guriddo.net/documentation/guriddo/javascript/user-guide/basic-grid/#options
+    /// </remarks>
     public class Grid
     {
+        // TODO: addOptions
+        // TODO: ajaxCellOptions
+        // TODO: ajaxGridOptions
+        // TODO: ajaxRowOptions
+        // TODO: ajaxSelectOptions
+        // TODO: ajaxSubgridOptions
+
+        /// <summary>
+        /// Specifies whether or not to zebra stripe alternate rows.
+        /// </summary>
+        [JsonProperty("altRows")]
+        public bool ShouldAlternateRowStyle { get; set; }
+
+        /// <summary>
+        /// Specifies whether or not request and response data should be HTML encoded.
+        /// </summary>
+        [JsonProperty("autoencode")]
+        public bool ShouldAutoEncode { get; set; }
+
+        /// <summary>
+        /// Specifies whether or not the grid's width is calculated automatically to the width
+        /// of the parent element on initial grid creation. In order to resize the grid when the
+        /// parent element or window changes, use the <see cref="Responsive"/> parameter instead.
+        /// </summary>
+        [JsonProperty("autowidth")]
+        public bool IsInitialWidthAutomatic { get; set; }
+
+        /// <summary>
+        /// The caption for the grid that appears above the column headers.
+        /// </summary>
+        /// <remarks>
+        /// http://www.guriddo.net/documentation/guriddo/javascript/#how-it-works
+        /// </remarks>
+        [JsonProperty("caption")]
+        public string Caption { get; set; }
+
+        /// <summary>
+        /// The padding + border width of the cell. Usually this should not be changed, but if custom
+        /// changes to the td element are made in the grid CSS, this will need to be adjusted.
+        /// </summary>
+        [JsonProperty("cellLayout")]
+        [DefaultValue(5)]
+        public int CellLayoutSize { get; set; } = 5;
+
+        /// <summary>
+        /// Specifies whether or not cell editing is enabled.
+        /// </summary>
+        [JsonProperty("cellEdit")]
+        public bool CanEdit { get; set; }
+
+        /// <summary>
+        /// Specifies where the contents of the cell should be saved.
+        /// In the case of <see cref="DataDestination.Remote"/>, the data is saved via an AJAX call.
+        /// </summary>
+        [JsonProperty("cellsubmit")]
+        [DefaultValue(DataDestination.Remote)]
+        public DataDestination CellSubmitDestination { get; set; } = DataDestination.Remote;
+
+        /// <summary>
+        /// The URL where the cell is saved when the <see cref="CellSubmitDestination"/> is set
+        /// to <see cref="DataDestination.Remote"/>.
+        /// </summary>
+        [JsonProperty("cellurl")]
+        public string CellSubmitUrl { get; set; }
+
+        // TODO: cmTemplate
+        // TODO: colFilters
+        // TODO: colMenu
+
+
+
         [JsonProperty("colNames")]
         public List<string> ColumnNames => Columns?.Select(c => c.Label ?? c.Name).ToList();
 
@@ -26,6 +101,11 @@ namespace WebHelpers.Mvc5.JqGrid
 
                 return Columns.Count(c => c.IsPrimaryKey) <= 1 &&
                        Columns.Count == ColumnNames.Count;
+            }
+
+            if (CellSubmitUrl != null && CellSubmitDestination != DataDestination.Remote)
+            {
+                return false;
             }
 
             return true;
