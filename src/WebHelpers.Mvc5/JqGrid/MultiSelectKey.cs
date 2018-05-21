04ee9fa4bdaa4f268493cfd2f230f32ba4f054b0
Connor O'Shea <connor@connoroshea.com>
2018-05-20 21:44:07 -0400

    Add more grid properties

---
+++ src/WebHelpers.Mvc5/JqGrid/MultiSelectKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WebHelpers.Mvc5.JqGrid
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum MultiSelectKey
+    {
+        [EnumMember(Value = "shiftKey")]
+        Shift,
+
+        [EnumMember(Value = "altKey")]
+        Alt,
+
+        [EnumMember(Value = "ctrlKey")]
+        Ctrl
+    }
+}
