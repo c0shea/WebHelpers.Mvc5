41915f4e895dda67beac3af08a8495cd68d22932
Connor O'Shea <connor@connoroshea.com>
2018-08-16 22:07:51 -0400

    Add ClientIP for HttpRequestBase (close #3)

--- src/WebHelpers.Mvc5/HttpRequestExtensions.cs
+++ src/WebHelpers.Mvc5/HttpRequestExtensions.cs
@@ -31,5 +31,29 @@ namespace WebHelpers.Mvc5
 
             return ip;
         }
+
+        /// <summary>
+        /// Gets the IP address of the client sending the request. This method will return the originating
+        /// IP if specified by a proxy but makes no guarantee that this is the client's true IP address.
+        /// Since these headers can be spoofed, you are encouraged to perform additional validation if
+        /// you are using the IP in a sensitive context.
+        /// </summary>
+        /// <param name="httpRequest">
+        /// The incoming request to get the client's IP address from.
+        /// This is typically from HttpContext.Current.Request or similar.
+        /// </param>
+        public static string ClientIP(this HttpRequestBase httpRequest)
+        {
+            var ip = httpRequest.Headers["X-Forwarded-For"] ??
+                     httpRequest.Headers["CF-Connecting-IP"] ??
+                     httpRequest.ServerVariables["REMOTE_HOST"];
+
+            if (ip.Contains(","))
+            {
+                ip = ip.Split(',').First().Trim();
+            }
+
+            return ip;
+        }
     }
 }
