4aca8f97be7bfbd1e55bcf5efaa73bba3b995e52
Connor O'Shea <connor@connoroshea.com>
2018-08-16 22:14:42 -0400

    Add version via BundleTransform (close #4)

--- src/WebHelpers.Mvc5/VersionExtensions.cs
+++ src/WebHelpers.Mvc5/VersionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Web.Hosting;
+using System.Web.Optimization;
 
 namespace WebHelpers.Mvc5
 {
@@ -16,6 +17,29 @@ namespace WebHelpers.Mvc5
         public static string AddVersion(this string contentPath)
         {
             return $"{contentPath}?v={File.GetLastWriteTime(HostingEnvironment.MapPath(contentPath)).Ticks}";
+        }
+
+        /// <summary>
+        /// Adds a cache-busting query parameter to the URL of the bundle.
+        /// The version is the number of ticks since the last write time of the file.
+        /// </summary>
+        /// <param name="bundle">The bundle (e.g. StyleBundle) to add the version to.</param>
+        public static Bundle AddVersion(this Bundle bundle)
+        {
+            bundle.Transforms.Add(new VersionBundleTransform());
+
+            return bundle;
+        }
+
+        private class VersionBundleTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                foreach (var file in response.Files)
+                {
+                    file.IncludedVirtualPath = $"{file.IncludedVirtualPath}?v={File.GetLastWriteTime(HostingEnvironment.MapPath(file.IncludedVirtualPath)).Ticks}";
+                }
+            }
         }
     }
 }
