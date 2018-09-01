bec038b1c40bd4b0ef695b706e84583a838acd57
Connor O'Shea <connor@connoroshea.com>
2018-09-01 17:37:15 -0400

    Add literal name and enum tests

--- tests/WebHelpers.Mvc5.Test/ConverterTests.cs
+++ tests/WebHelpers.Mvc5.Test/ConverterTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using WebHelpers.Mvc5.JqGrid.Converters;
@@ -17,10 +18,53 @@ namespace WebHelpers.Mvc5.Test
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void LiteralNameConverter()
+        {
+            var actual = JsonConvert.SerializeObject(new LiteralNameConverterTest());
+            var expected = @"{""LiteralNameProperty"":JsMethod}";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LiteralEnumConverter()
+        {
+            var actual = JsonConvert.SerializeObject(new LiteralEnumConverterTest());
+            var expected = @"{""One"":1,""Two"":Second,""Three"":Three}";
+
+            Assert.AreEqual(expected, actual);
+        }
+
         private class TimeSpanConverterTest
         {
             [JsonConverter(typeof(TimeSpanConverter))]
             public TimeSpan TimeSpanProperty { get; set; } = TimeSpan.FromMilliseconds(123);
+        }
+
+        private class LiteralNameConverterTest
+        {
+            [JsonConverter(typeof(LiteralNameConverter))]
+            public string LiteralNameProperty { get; set; } = "JsMethod";
+        }
+
+        private class LiteralEnumConverterTest
+        {
+            public LiteralEnum One { get; set; } = LiteralEnum.One;
+            public LiteralEnum Two { get; set; } = LiteralEnum.Two;
+            public LiteralEnum Three { get; set; } = LiteralEnum.Three;
+        }
+
+        [JsonConverter(typeof(LiteralEnumConverter))]
+        private enum LiteralEnum
+        {
+            [EnumMember(Value = "1")]
+            One,
+
+            [EnumMember(Value = "Second")]
+            Two,
+            
+            Three
         }
     }
 }
